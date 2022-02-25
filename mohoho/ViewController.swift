b4dcf6fa51c319a5c651b508210209fd8ef22b99
jonipark <park49j@mtholyoke.edu>
2022-02-25 13:56:57 -0500

    Initial Commit

---
+++ mohoho/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mohoho
+//
+//  Created by Joeun Park on 2/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
